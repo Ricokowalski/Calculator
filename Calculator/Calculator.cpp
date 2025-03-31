76d76053c0ff7d40aa42c48f4eecfb740e3fc9b9
Weichenrambo <ricokowalski.02@gmail.com>
2025-03-31 16:17:39 +0200

    Grundaufbau erweitert

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+using namespace std;
 
 typedef enum calcOp{
     add,
     sub,
     mult,
-    div
+    divi
 } calcOperations;
 
 struct CalculationStructure {
+    unsigned int pos;
     int val_1, val_2, res;
     calcOperations calcOp;
 };
@@ -16,7 +18,31 @@ typedef struct CalculationStructure CalculationStructureType;
 
 CalculationStructureType CalculationMemory = {};
 
+void calculate_result(CalculationStructureType* data) {
 
+    switch (data->calcOp)
+    {
+    case add:
+        data->res = data->val_1 + data->val_2;
+        break;
+    case sub:
+        data->res = data->val_1 - data->val_2;
+        break;
+    case mult:
+        data->res = data->val_1 * data->val_2;
+        break;
+    case divi:
+        data->res = data->val_1 / data->val_2;
+        break;
+    default:
+        cout << "Fehler aufgetreten! Berechnungsoperator stimmt nicht!";
+        break;
+    }
+}
+
+void init_new_CalcStruct() {
+
+}
 
 int main()
 {
